5318fc7f8f36e1b9da8e0e75378178eae8aa126a
bachung <bachung@gmail.com>
2023-10-19 00:46:30 -0700

    forgot to commit ts module

---
+++ src/modules/pure-color.d.ts
@@ -0,0 +1,17 @@
+declare module 'pure-color/convert/rgb2hsl' {
+    function rgb2hsl(rgb: [number, number, number]): [number, number, number];
+
+    export = rgb2hsl;
+}
+
+declare module 'pure-color/convert/hsl2rgb' {
+    function hsl2rgb(hsl: [number, number, number]): [number, number, number];
+
+    export = hsl2rgb;
+}
+
+declare module 'pure-color/convert/rgb2hex' {
+    function rgb2hex(rgb: [number, number, number]): string;
+
+    export = rgb2hex;
+}
