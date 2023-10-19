3cfc2eb9be5abe29bbe462443558795e2f35f473
bachung <bachung@gmail.com>
2023-10-19 08:51:47 -0700

    Add custom emoji uploader

--- src/components/BearPicker.tsx
+++ src/components/BearPicker.tsx
@@ -7,11 +7,34 @@ type Props = {
 };
 
 const BearPicker = ({bear, onChange}: Props) => {
+    const [uploaded, setUploaded] = React.useState([] as Bear[]);
     return (
-        <div style={{maxWidth: 540, display: 'flex', flexDirection: 'row', flexWrap: 'wrap', gap: 8}}>
-            {Bears.map((bear, idx) => {
-                return <img width={100} height={100} src={bear.url} key={idx} alt={bear.name} onClick={() => onChange(bear)} style={{cursor: 'pointer'}} />;
-            })}
+        <div style={{display: 'flex', flexDirection: 'column', alignContent: 'start'}}>
+            <div style={{maxWidth: 540, display: 'flex', flexDirection: 'row', flexWrap: 'wrap', gap: 8}}>
+                {Bears.concat(uploaded).map((bear, idx) => {
+                    return <img width={100} height={100} src={bear.url} key={idx} alt={bear.name} onClick={() => onChange(bear)} style={{cursor: 'pointer'}} />;
+                })}
+            </div>
+            <div style={{display: 'flex', flexDirection: 'row', gap: 8, alignItems: 'start'}}>
+                Upload custom emoji
+                <input type="file" accept="image/png" onChange={e => {
+                    const file = (e.target.files ?? [])[0];
+                    if (file != null) {
+                        const fr = new FileReader();
+                        fr.readAsDataURL(file);
+                        fr.onload = () => {
+                            const result = fr.result;
+                            if (result != null && typeof result === 'string') {
+                                setUploaded(uploaded => ([...uploaded, {
+                                    name: `Custom ${uploaded.length + 1}`,
+                                    url: result
+                                }]));
+                            }
+                        };
+                        
+                    }
+                }}/>
+            </div>
         </div>
     )
 };
